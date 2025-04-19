d90291f81ca7995a2b9039ec3ab1030f6db72db2
Veythos <Veythos@proton.me>
2025-04-19 19:47:28 +0330

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']), // تنظیم مقدار APP_KEYS
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
